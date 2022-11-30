c81cbdf4c04c3288fe434f8a65124e38360ff30a
Sayrarh <ajaotosinserah@gmail.com>
2022-11-30 05:05:12 +0100

    state

--- contracts/Payroll.sol
+++ contracts/Payroll.sol
@@ -2,5 +2,30 @@
 pragma solidity ^0.8.1;
 
 contract Payroll {
-    
+
+    enum EmployeeLevel{
+        Junior,
+        Intermediate,
+        Senior
+    }
+
+    struct EmployeeInfo{
+        string name;
+        address employeeAddress;
+        string post;
+        EmployeeLevel level;
+        uint timeFilled;
+    }
+
+    struct SalaryInvoice{
+        string name;
+        address employeeAddress;
+        string post;
+        EmployeeLevel level;
+        uint time;
+        uint amountTobepaid;
+        uint ratePerDay;
+        string description;
+        uint extraWorkFee;
+    }
 }
